e33e51e21513fef143d7d93ea332cb293e621ec1
sourav <souravkumar59139@gmail.com>
2025-02-09 21:31:10 +0530

    pair concept

---
+++ Reverse.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print (int n){
+    for(int i=0; i<=n; i++){
+        for(int j=1; j<=n-i+1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int t;
+    cin>>t;
+    for(int i=0; i<=t; i++){
+        int n;
+        cin>>n;
+        print(n);
+    }
+}
---
+++ pair.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainPair (){
+    //pair<int,int>p={2,4};
+    //cout<<p.first<<" "<<p.second;
+    
+   // pair<int,pair<int,int>> p = {11,{4,5}};
+   // cout<<p.first<<" "<<p.second.second<<" "<<p.second.first;
+   
+   pair<int,int>arr[] = {{2,3}, {5,6},{54,34}, {12,23}};
+   cout<<arr[0].second;
+}
+
+int main(){
+    explainPair();
+   return 0;
+}

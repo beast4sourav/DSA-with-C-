042d0d74179bbbaf34c10a9db7ee841a32ed383e
sourav <souravkumar59139@gmail.com>
2025-02-11 23:46:23 +0530

    Armstrong number

---
+++ armstrong.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int sum = 0;
+    cin>>n;
+    while(n>0){
+        int lastD = n%10;
+        sum = sum + (lastD*lastD*lastD);
+        n=n/10;
+    }
+    cout<<sum;
+}

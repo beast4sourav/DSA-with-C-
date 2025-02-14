2c87348d8ef479a655b77ece399062e743d94036
sourav <souravkumar59139@gmail.com>
2025-02-14 23:56:03 +0530

    Recusion

---
+++ Recusion.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void test(int count){
+    if(count == 4){
+        return;
+    }
+    cout << count << " ";
+    test(count + 1);
+}
+
+int main() {
+    test(0);  // Start the count from 0
+    return 0;
+}

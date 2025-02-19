1cceb7f2e07ffd5b29fe151989efde33dfd1c581
sourav <souravkumar59139@gmail.com>
2025-02-19 19:58:29 +0530

    reverse arr

---
+++ Nto1Back.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+void f(int i, int n){
+    if(i>n){
+        return;
+    }
+    f(i+1,n);
+    cout<<i<<" ";
+     
+    
+}
+
+int main() {
+    int n;
+    cin>>n;
+    f(1,n);
+    return 0;
+}
